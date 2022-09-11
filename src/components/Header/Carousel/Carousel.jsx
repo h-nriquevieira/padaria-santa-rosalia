d2da7a847cbaa7d6a10c39e9e836a9cec9c04e6c
h-nriquevieira <h.nriquevieira@gmail.com>
2022-09-11 15:04:32 -0300

    Implement react-slick as hero slider

---
+++ src/components/Header/Carousel/Carousel.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import './Carousel.css'
+import Slider from 'react-slick'
+import slide1Mobile from '../../../assets/slide-1-mobile.jpg'
+import slide2Mobile from '../../../assets/slide-2-mobile.jpg'
+import slide3Mobile from '../../../assets/slide-3-mobile.jpg'
+import slide4Mobile from '../../../assets/slide-4-mobile.jpg'
+import slide1 from '../../../assets/slide-1.jpg'
+import slide2 from '../../../assets/slide-2.jpg'
+import slide3 from '../../../assets/slide-3.jpg'
+import slide4 from '../../../assets/slide-4.jpg'
+import { useMediaQuery } from 'react-responsive';
+
+function Carousel() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autplaySpeed: 300,
+    fade: true
+  }
+
+  const isBigScreen = useMediaQuery(
+    {
+      query: "(min-width: 1024px)"
+    }
+  )
+  
+  return (
+    <>
+
+    {isBigScreen ? 
+    <Slider {...settings} className="slider">
+      <div><img src={slide1} alt="" /></div>
+      <div><img src={slide2} alt="" /></div>
+      <div><img src={slide3} alt="" /></div>
+      <div><img src={slide4} alt="" /></div>
+    </Slider>
+    :<Slider {...settings} className="slider">
+      <div><img src={slide1Mobile} alt="" /></div>
+      <div><img src={slide2Mobile} alt="" /></div>
+      <div><img src={slide3Mobile} alt="" /></div>
+      <div><img src={slide4Mobile} alt="" /></div>
+    </Slider>}
+    </>
+  );
+}
+
+export default Carousel;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,29 @@ import './Header.css'
 
 import logo from '../../assets/logo.png'
 import arrow from '../../assets/header--arrow-icon.svg'
+import Carousel from './Carousel/Carousel';
 
 
 function Header() {
   return (
       <header>
         <img src={logo} alt="" className="header--logo" />
-        <div className="header--hero">
+        {/* <div className="header--hero">
           <h2>Proporcione a <span className="bold">melhor experiência</span> para seus convidados</h2>
           <button className='header--btn'>
             <img src={arrow} alt="" />
             <a href="">Encomende seu coffee break</a>
           </button>
+        </div> */}
+        <div className="header--slider">
+          <Carousel />
+          <div className="header--overlay-text">
+            <h2>Proporcione a <span className="bold">melhor experiência</span> para seus convidados</h2>
+            <button className='header--btn'>
+              <img src={arrow} alt="" />
+              <a href="">Encomende seu coffee break</a>
+            </button>
+          </div>
         </div>
         <div className="header--scrolling-text">
           <span>Coffee Break</span>
